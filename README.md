74d043193ecc5b5b88470c88575125d74fc019ab
Craig Johnston <cjimti@gmail.com>
2019-02-15 12:56:48 -0800

    basic operation

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
-# /etc/hosts manager library
+# Etc Hosts Management Library
 
-WIP
+This small go library was developed to encapsulate the complexity of
+working with /etc/hosts by providing a simple interfact to load, add or remove rntries and save a /etc/host file. No validation is done on the input data and is considered out of scope for this project.
+
+Basic implemention:
+```go
+
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/txn2/txeh"
+)
+
+func main() {
+	hosts := txeh.NewHostsDefault()
+
+	hosts.AddHost("127.100.100.100", "test")
+	hosts.AddHost("127.100.100.101", "logstash")
+	hosts.AddHosts("127.100.100.102", []string{"a", "b", "c"})
+	
+	hosts.RemoveHosts([]string{"example", "example.machine", "example.machine.example.com"})
+	hosts.RemoveHosts(strings.Fields("example2 example.machine2 example.machine.example.com2"))
+
+	
+	hosts.RemoveAddress("127.1.27.1")
+	
+	removeList := []string{
+		"127.1.27.15",
+		"127.1.27.14",
+		"127.1.27.13",
+	}
+	
+	hosts.RemoveAddresses(removeList)
+	
+	hfData := hosts.RenderHostsFile()
+
+	fmt.Println(hfData)
+	
+	hosts.SaveAs("./test.hosts")
+}
+
+```
--- hosts.go
+++ hosts.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"runtime"
 	"strings"
+	"sync"
 )
 
 const UNKNOWN = 0
@@ -14,12 +16,14 @@ const ADDRESS = 30
 
 // HostsConfig
 type HostsConfig struct {
-	FilePath string
+	ReadFilePath  string
+	WriteFilePath string
 }
 
 // Hosts
 type Hosts struct {
-	HostsConfig
+	sync.Mutex
+	*HostsConfig
 	hostFileLines HostFileLines
 }
 
@@ -44,32 +48,115 @@ type HostFileLine struct {
 	Trimed          string
 }
 
-// NewHosts returns a new hosts object
-func NewHosts(hc HostsConfig) *Hosts {
-	h := &Hosts{HostsConfig: hc}
+// NewHostsDefault returns a hosts object with
+// default configuration
+func NewHostsDefault() *Hosts {
+	return NewHosts(&HostsConfig{})
+}
 
-	if h.FilePath == "" {
-		h.FilePath = "/etc/hosts"
+// NewHosts returns a new hosts object
+func NewHosts(hc *HostsConfig) *Hosts {
+	h := &Hosts{HostsConfig: hc}
+	h.Lock()
+	defer h.Unlock()
+
+	defaultHostsFile := "/etc/hosts"
+
+	if runtime.GOOS == "windows" {
+		defaultHostsFile = `C:\Windows\System32\Drivers\etc\hosts`
 	}
 
-	h.hostFileLines = ParseHosts(h.FilePath)
+	if h.ReadFilePath == "" {
+		h.ReadFilePath = defaultHostsFile
+	}
+
+	if h.WriteFilePath == "" {
+		h.WriteFilePath = h.ReadFilePath
+	}
+
+	h.hostFileLines = ParseHosts(h.ReadFilePath)
 
 	return h
 }
 
-// AddHost adds a host to an address and removes the host
-// from any existing address is may be associated with
-func (h *Hosts) AddHost(address string, host string) {
+// Save rendered hosts file
+func (h *Hosts) Save() error {
+	return h.SaveAs(h.WriteFilePath)
+}
 
-	// does the host already exist
-	if ok, exAdd, hflIdx := h.HostAddressLookup(host); ok {
-		// if the address is the same we are done
-		if address == exAdd {
-			log.Print("Address / Host combination already exists. Do nothing.")
-			return
+// SaveAs saves rendered hosts file to the filename specified
+func (h *Hosts) SaveAs(fileName string) error {
+	hfData := []byte(h.RenderHostsFile())
+
+	err := ioutil.WriteFile(fileName, hfData, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Reload hosts file
+func (h *Hosts) Reload() {
+	h.Lock()
+	defer h.Unlock()
+
+	h.hostFileLines = ParseHosts(h.ReadFilePath)
+}
+
+// RemoveAddresses removes all entries (lines) with the provided address.
+func (h *Hosts) RemoveAddresses(addresses []string) {
+	for _, address := range addresses {
+		if h.RemoveFirstAddress(address) {
+			h.RemoveAddress(address)
 		}
+	}
+}
 
-		// if the hostname is at a different address, go and remove it from the address
+// RemoveAddress removes all entries (lines) with the provided address.
+func (h *Hosts) RemoveAddress(address string) {
+	if h.RemoveFirstAddress(address) {
+		h.RemoveAddress(address)
+	}
+}
+
+// RemoveFirstAddress removed the first entry (line) found with the provided address.
+func (h *Hosts) RemoveFirstAddress(address string) bool {
+	h.Lock()
+	defer h.Unlock()
+
+	for hflIdx := range h.hostFileLines {
+		if address == h.hostFileLines[hflIdx].Address {
+			h.hostFileLines = removeHFLElement(h.hostFileLines, hflIdx)
+			return true
+		}
+	}
+
+	return false
+}
+
+// RemoveHosts removes all hostname entries of the provided host slice
+func (h *Hosts) RemoveHosts(hosts []string) {
+	for _, host := range hosts {
+		if h.RemoveFirstHost(host) {
+			h.RemoveHost(host)
+		}
+	}
+}
+
+// RemoveHost removes all hostname entries of provided host
+func (h *Hosts) RemoveHost(host string) {
+	if h.RemoveFirstHost(host) {
+		h.RemoveHost(host)
+	}
+}
+
+// RemoveHost the first hostname entry found and returns true if successful
+func (h *Hosts) RemoveFirstHost(host string) bool {
+	h.Lock()
+	defer h.Unlock()
+
+	for hflIdx := range h.hostFileLines {
 		for hidx, hst := range h.hostFileLines[hflIdx].Hostnames {
 			if hst == host {
 				h.hostFileLines[hflIdx].Hostnames = removeStringElement(h.hostFileLines[hflIdx].Hostnames, hidx)
@@ -78,6 +165,49 @@ func (h *Hosts) AddHost(address string, host string) {
 				if len(h.hostFileLines[hflIdx].Hostnames) < 1 {
 					h.hostFileLines = removeHFLElement(h.hostFileLines, hflIdx)
 				}
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+// AddHosts adds an array of hosts to the first matching address it finds
+// or creates the address and adds the hosts
+func (h *Hosts) AddHosts(address string, hosts []string) {
+	for _, hst := range hosts {
+		h.AddHost(address, hst)
+	}
+}
+
+// AddHost adds a host to an address and removes the host
+// from any existing address is may be associated with
+func (h *Hosts) AddHost(addressRaw string, hostRaw string) {
+	host := strings.TrimSpace(strings.ToLower(hostRaw))
+	address := strings.TrimSpace(strings.ToLower(addressRaw))
+
+	// does the host already exist
+	if ok, exAdd, hflIdx := h.HostAddressLookup(host); ok {
+		// if the address is the same we are done
+		if address == exAdd {
+			return
+		}
+
+		// if the hostname is at a different address, go and remove it from the address
+		for hidx, hst := range h.hostFileLines[hflIdx].Hostnames {
+			if hst == host {
+				h.Lock()
+				h.hostFileLines[hflIdx].Hostnames = removeStringElement(h.hostFileLines[hflIdx].Hostnames, hidx)
+				h.Unlock()
+
+				// remove the address line if empty
+				if len(h.hostFileLines[hflIdx].Hostnames) < 1 {
+					h.Lock()
+					h.hostFileLines = removeHFLElement(h.hostFileLines, hflIdx)
+					h.Unlock()
+				}
+
 				break // unless we should continue because it could have duplicates
 			}
 		}
@@ -86,7 +216,9 @@ func (h *Hosts) AddHost(address string, host string) {
 	// if the address exists add it to the address line
 	for i, hfl := range h.hostFileLines {
 		if hfl.Address == address {
+			h.Lock()
 			h.hostFileLines[i].Hostnames = append(h.hostFileLines[i].Hostnames, host)
+			h.Unlock()
 			return
 		}
 	}
@@ -98,12 +230,17 @@ func (h *Hosts) AddHost(address string, host string) {
 		Hostnames: []string{host},
 	}
 
+	h.Lock()
 	h.hostFileLines = append(h.hostFileLines, hfl)
+	h.Unlock()
 }
 
 // HostAddressLookup returns true is the host is found, a string
 // containing the address and the index of the hfl
 func (h *Hosts) HostAddressLookup(host string) (bool, string, int) {
+	h.Lock()
+	defer h.Unlock()
+
 	for i, hfl := range h.hostFileLines {
 		for _, hn := range hfl.Hostnames {
 			if hn == strings.ToLower(host) {
@@ -117,6 +254,9 @@ func (h *Hosts) HostAddressLookup(host string) (bool, string, int) {
 
 // RenderHostsFile
 func (h *Hosts) RenderHostsFile() string {
+	h.Lock()
+	defer h.Unlock()
+
 	hf := ""
 
 	for _, hfl := range h.hostFileLines {
@@ -166,8 +306,12 @@ func ParseHosts(path string) []HostFileLine {
 
 		if len(curLine.Parts) > 1 {
 			curLine.LineType = ADDRESS
-			curLine.Address = curLine.Parts[0]
-			curLine.Hostnames = curLine.Parts[1:]
+			curLine.Address = strings.ToLower(curLine.Parts[0])
+			// lower case all
+			for _, p := range curLine.Parts[1:] {
+				curLine.Hostnames = append(curLine.Hostnames, strings.ToLower(p))
+			}
+
 			continue
 		}
 
@@ -197,5 +341,5 @@ func lineFormatter(hfl HostFileLine) string {
 		return hfl.Raw
 	}
 
-	return fmt.Sprintf("%-15s %s", hfl.Address, strings.Join(hfl.Hostnames, " "))
+	return fmt.Sprintf("%-16s %s", hfl.Address, strings.Join(hfl.Hostnames, " "))
 }
