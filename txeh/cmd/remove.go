91117c39011a3628d7875172a20ee700c3dc9e25
cjimti <cj@imti.co>
2026-02-04 19:56:05 -0800

    housekeeping: discard unused arguments in remove.go command function

--- txeh/cmd/remove.go
+++ txeh/cmd/remove.go
@@ -15,7 +15,7 @@ var removeCmd = &cobra.Command{
 	Use:   "remove [TYPE] [HOSTNAME|IP] [HOSTNAME|IP] [HOSTNAME|IP]...",
 	Short: "Remove a hostname or ip address",
 	Long:  `Remove one or more hostnames or ip addresses from /etc/hosts`,
-	Run: func(cmd *cobra.Command, args []string) {
+	Run: func(cmd *cobra.Command, _ []string) {
 		err := cmd.Help()
 		if err != nil {
 			fmt.Printf("Error: can not display help, reason: %s\n", err.Error())
