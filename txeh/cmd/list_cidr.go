c7bf96040f188c1f9131d583794351554661ed08
cjimti <cj@imti.co>
2026-02-04 19:55:37 -0800

    housekeeping: refactor list_cidr.go to discard unused arguments and add a function comment

--- txeh/cmd/list_cidr.go
+++ txeh/cmd/list_cidr.go
@@ -15,7 +15,7 @@ var listByCidrCmd = &cobra.Command{
 	Use:   "cidr [CIDR] [CIDR] [CIDR]...",
 	Short: "List hosts for one or more CIDRs",
 	Long:  `List hosts for one or more CIDRs from /etc/hosts`,
-	Args: func(cmd *cobra.Command, args []string) error {
+	Args: func(_ *cobra.Command, args []string) error {
 		if len(args) < 1 {
 			return errors.New("the \"list cidr\" command requires at least one CIDR address")
 		}
@@ -26,11 +26,12 @@ var listByCidrCmd = &cobra.Command{
 
 		return nil
 	},
-	Run: func(cmd *cobra.Command, args []string) {
+	Run: func(_ *cobra.Command, args []string) {
 		ListByCIDRs(args)
 	},
 }
 
+// ListByCIDRs lists hostnames for the given CIDR ranges.
 func ListByCIDRs(cidrs []string) {
 	for _, cidr := range cidrs {
 		ipHosts := etcHosts.ListHostsByCIDR(cidr)
